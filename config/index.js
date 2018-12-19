1b403fd647d89b9c6ce1ebaa8cb0a5819451d64a
hani.run <hani.run@kakaocorp.com>
2018-12-19 18:24:20 +0900

    -

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ module.exports = {
   index: path.resolve(__dirname, '../docs/index.html'),
   assetsRoot: path.resolve(__dirname, '../docs'),
   assetsSubDirectory: 'static',
-  assetsPublicPath: '',
+  assetsPublicPath: '/docs/',
   productionSourceMap: true,
   devtool: '#source-map',
   productionGzip: false,
