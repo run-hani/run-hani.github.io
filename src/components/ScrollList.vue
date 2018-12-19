1156d6c67a0c69839a7209ad5f6e6147ec3412a9
hani.run <hani.run@kakaocorp.com>
2018-12-19 22:33:43 +0900

    -

--- src/components/ScrollList.vue
+++ src/components/ScrollList.vue
@@ -8,26 +8,26 @@
     <ul class="list_pages">
       <li>
           <strong class="tit_pages">#1.</strong>
-          <span class="txt_pages">휠스크롤 시 한페이지씩 내려가는 형태입니다.</span>
+          <span class="txt_pages">휠스크롤 시 해당영역의 네비게이션이 활성화 됩니다. ex. Bootstrap Scrollspy</span>
           <div class="group_link">
-            <a href="#none" class="link_page">가로형</a>
-            <a href="#none" class="link_page">세로형</a>
+            <a href="https://codepen.io/run-hani/full/bOemoj" target="_blank" class="link_page">가로형</a>
+            <a href="https://codepen.io/run-hani/full/VqeVNw" target="_blank" class="link_page">세로형</a>
           </div>
       </li>
       <li>
           <strong class="tit_pages">#2.</strong>
-          <span class="txt_pages">휠스크롤 시 아래에서 한페이지씩 올라오는 형태입니다.</span>
+          <span class="txt_pages">휠스크롤 시 한페이지씩 아래로 이동합니다.</span>
           <div class="group_link">
-            <a href="#none" class="link_page">가로형</a>
-            <a href="#none" class="link_page">세로형</a>
+            <a href="https://codepen.io/run-hani/full/NerOrM" target="_blank" class="link_page">가로형</a>
+            <a href="https://codepen.io/run-hani/full/NexVWE" target="_blank" class="link_page">세로형</a>
           </div>
       </li>
       <li>
           <strong class="tit_pages">#3.</strong>
-          <span class="txt_pages">스크롤 스파이만 적용된 형태입니다.</span>
+          <span class="txt_pages">휠스크롤 시 아래에서 한페이지씩 올라옵니다.</span>
           <div class="group_link">
-            <a href="#none" class="link_page">가로형</a>
-            <a href="#none" class="link_page">세로형</a>
+            <a href="https://codepen.io/run-hani/full/xmOyWV" target="_blank" class="link_page">가로형</a>
+            <a href="https://codepen.io/run-hani/full/KbgBVj" target="_blank" class="link_page">세로형</a>
           </div>
       </li>
     </ul>
