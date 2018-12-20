b659b80611044d0c8ed73fba59037cf4d8d1a2ed
hani.run <hani.run@kakaocorp.com>
2018-12-20 17:04:41 +0900

    [181220] 사이트 리스트 추가

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -7,7 +7,7 @@
 
 	<header class="head_games">
 		<h1 class="logo_games">
-			<router-link to="/" class="link_logo" exact>
+			<router-link to="/docs/" class="link_logo" exact>
 				<img src="../../assets/img/logo_games.png" class="img_logo" alt="dk techin">
 					<span class="txt_logo">모션 가이드</span>
 			</router-link>
